c47d8638effee35e4386be5408f88c8914531646
riturajborpujari@outlook.com <riturajborpujari@outlook.com>
2020-06-03 18:23:45 +0530

    BUG: access_token needs to be access_token_key

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ const twitter_conf = {
   acc: {
     consumer_key: process.env.consumer_key,
     consumer_secret: process.env.consumer_secret,
-    access_token: process.env.access_token,
+    access_token_key: process.env.access_token_key,
     access_token_secret: process.env.access_token_secret
   } 
 }
